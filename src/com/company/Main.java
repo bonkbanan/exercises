d2a81a6a85b3be8bc2b0f57ab8f194e939817e05
dench <denchikbalan@gmail.com>
2021-04-17 10:51:28 +0300

    b

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,11 +21,22 @@ public class Main {
         return (Arrays.stream(array).boxed().distinct()
                 .count() != array.length);
     }
-//hasDuplicate == hasDuplicate2
+
+    public static Boolean hasDuplicates3(int[] array){
+        Arrays.sort(array);
+        for (int i = 0; i < array.length-1; i++) {
+            if(array[i]==array[i + 1])
+                return true;
+        }
+        return false;
+    }
+
+//hasDuplicate == hasDuplicate2 == hasDuplicate3
     public static void main(String[] args) {
 	    int[] array= {1, 2, 3, 5, 465, 2, 8, 6, 8};
         System.out.println(hasDuplicates(array));
         System.out.println(hasDuplicates2(array));
+        System.out.println(hasDuplicates3(array));
 
         String s1 = "Hello World";
         StringBuilder sb= new StringBuilder(s1);
